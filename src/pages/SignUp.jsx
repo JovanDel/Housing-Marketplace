ba88466e1fe59b1dcd60191f0a5853b71d38cc0d
Jovan Delport <jovan@imgn.africa>
2022-09-04 21:47:53 +0200

    Minor change to SignUp

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,11 +1,11 @@
-import {useState} from 'react';
-import {Link, useNavigate} from 'react-router-dom';
-import {toast} from 'react-toastify';
-import{getAuth, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth';
-import {setDoc, doc, serverTimestamp} from 'firebase/firestore';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import{ getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { setDoc, doc, serverTimestamp } from 'firebase/firestore';
 import { db } from '../firebase.config'
 import OAuth from '../components/OAuth';
-import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg';
 
 function SignUp() {
