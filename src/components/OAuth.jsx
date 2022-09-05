8b9d2599bb4dd3407bd184f1a9d6ad5564c84b0a
Jovan Delport <jovan@imgn.africa>
2022-09-05 12:19:20 +0200

    Changes to authentication

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -11,38 +11,36 @@ function OAuth() {
 
     const onGoogleClick = async () => {
         try {
-            const auth =getAuth()
-            const provider = new GoogleAuthProvider()
-            const result = await signInWithPopup(auth, provider)
-            const user = result.user
-
-            // Check for user
-            const docRef = doc(db, 'users', user.uid)
-            const docSnap = await getDoc(docRef)
-
-            // If user, doesn't exist, create user
-            if(!docSnap.exists()) {
-                await setDoc(doc(db, 'users', user.uid), {
-                    name: user.displayName,
-                    email: user.email,
-                    timestamp: serverTimestamp(),
-                })
-            }
+           const auth = getAuth()
+           const provider = new GoogleAuthProvider()
+           const result = await signInWithPopup(auth, provider)
+           const user = result.user
+           
+           // Check for user
+           const docRef = doc(db, 'users', user.uid)
+           const docSnap = await getDoc(docRef)
+           
+           // If user doesn't exists, create user
+           if(!docSnap.exists()) {
+            await setDoc(doc('users', user.uid), {
+                name: user.displayName,
+                email: user.email,
+                timestamp: serverTimestamp()
+            })
+           } 
             navigate('/')
-        } catch (error) {
-            toast.error('Could not authorize with Google')
-            
+           } catch (error) {
+             toast.error('Could not authorize with Google')           
         }
     }
 
     return (
-        <div className='socialLogin'>
-            <p>Sign {location.pathname === '/sign-up' ? 'up' : 'in'} 
-             with  </p>
-            <button className="socialIconDiv" onClick={onGoogleClick}>
-                <img className='socialIconImg' src={googleIcon} alt='google' />
-            </button>
-        </div>
+       <div className='socialLogin'>
+        <p>Sign {location.pathname === '/sign-up' ? 'up' : 'in'} with </p>
+        <button className="socialIconDiv" onClick={onGoogleClick}>
+            <img className='socialIconImg' src={googleIcon} alt="google" />
+        </button>
+       </div>
     )
 }
 
